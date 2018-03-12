ff8353252f117e87ef9ef0f4961c09f31311a50c
Matt Wigdahl <mlwigdahl@gmail.com>
2018-03-11 22:55:58 -0500

    half-baked start on recursive descent parser...

--- src/i7parse.js
+++ src/i7parse.js
@@ -1,8 +1,39 @@
 
 // the parser will go here
 
+let cur;
+let gTokens;
+let gInd = 0;
+
+function ntok() {
+    if (gInd == gTokens.length) {
+        error(`ntok: out of tokens at ${gInd}`);
+    }
+    cur = gTokens[gInd++];
+}
+
+function error(err) {
+    console.log(err);
+}
+
+function accept(tok) {
+    if (cur == tok) {
+        ntok();
+        return true;
+    }
+    return false;
+}
+
+function expect(tok) {
+    if (accept(tok)) return true;
+    error("expect: unexpected symbol");
+    return false;
+}
+
 function parse(tokens, grammar)
 {
+    // set up the globals
+    gTokens = tokens;
     return;
 }
 
--- src/i7parsegrammar.js
+++ src/i7parsegrammar.js
@@ -1,6 +1,29 @@
 
+// using RD parsing
 
+// grammar structure is:
 
-const grammar = [];
+/*
+ source
+    title pb
+    paragraph { pb paragraph }
+
+title
+    qt
+    qt 'by' words
+    qt 'by' qt
+
+qt
+    '"' words '"'
+
+words
+    tok
+    tok words
+*/ 
+
+const grammar = [
+
+];
+
 
 export default grammar;
